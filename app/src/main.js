06e54c267059a6978e1c9c2d4fe189af5d120664
parkkwa6 <kwangjin.park@mail.utoronto.ca>
2020-03-04 21:52:17 -0500

    Vue Axios Demo

--- app/src/main.js
+++ app/src/main.js
@@ -4,16 +4,15 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
-import '../node_modules/bulma/css/bulma.css'
-import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = falsew
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import BootstrapVue from '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
+import '../node_modules/bulma/css/bulma.css'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
 
-Vue.use(VueAxios, axios, Vuex, VueRouter, BootstrapVue, vuetify)
+Vue.use(VueAxios, axios, Vuex, VueRouter, BootstrapVue, Vuetify)
 
 new Vue({
   render: h => h(App),
