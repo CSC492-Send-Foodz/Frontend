da1cf0fead0899957dd786ff6d253c4666abcc07
Nada Elnour <nelnour90@gmail.com>
2020-03-05 21:19:02 -0500

    Merge master dependencies

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { BootstrapVue } from 'bootstrap-vue';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
-import '../node_modules/bulma/css/bulma.css';
-import vuetify from './plugins/vuetify';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
+import '../node_modules/bulma/css/bulma.css'
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios, Vuex, VueRouter, BootstrapVue, vuetify)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
 
 Vue.config.productionTip = false
 
