adfc1b0fba06ff8ab49de783c492d1943efe5aa9
Nada Elnour <nelnour90@gmail.com>
2020-03-08 16:44:05 -0400

    Set up websocket listener

--- app/src/components/ActiveOrdersTable.vue
+++ app/src/components/ActiveOrdersTable.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div @load="sendStoreId">
     <v-data-table
       :headers="fields"
       :items="activeOrders"
       class="elevation-1"
-      hide
-      actions
+      hide actions
+      :hide-default-footer="true"
     >
       <template v-slot:item="row">
         <tr>
@@ -32,9 +32,12 @@
   </div>
 </template>
 <script>
+import io from 'socket.io-client';
+
 export default {
   data() {
     return {
+      socket : io('localhost:3001'),
       activeOrders: [
         {
           EDINumber: 3232,
@@ -42,9 +45,7 @@ export default {
           time: Date(),
           orderStatus: "Searching for Driver",
           items: [
-            { name: "apple", qty: 45 },
-            { name: "orange", qty: 100 },
-            { name: "banana", qty: 60 }
+            {name: "apple", qty: 45}
           ]
         },
         {
@@ -53,9 +54,7 @@ export default {
           time: Date(),
           orderStatus: "Picked up",
           items: [
-            { name: "guava", qty: 22 },
-            { name: "pineapple", qty: 500 },
-            { name: "acorn", qty: 40 }
+            {name: "pineapple", qty: 1000}
           ]
         }
       ],
@@ -82,115 +81,20 @@ export default {
       return false;
     },
     getInventory() {
-
+      this.activeOrders = [];
+      this.socket.on("INVENTORY_DATA", items => {
+        items.forEach(item => {
+          this.activeOrders.push(item);
+        });
+      });
     },
-    sendStatus() {
-
+    sendStoreId(event) {
+      event.preventDefault();
+      // send storeID to back end to get inventory
+      this.socket.emit("STORE_ID", {
+        param: this.$store.state.id
+      });
     }
-
-  }
-};
-</script>
-<style lang="scss" scoped>
-#expand,
-#response {
-  background-color: white;
-  color: #333;
-  border: none;
-  outline: none;
-}
-</style>
-
-<template>
-  <div>
-    <v-data-table
-      :headers="fields"
-      :items="activeOrders"
-      class="elevation-1"
-      hide
-      actions
-    >
-      <template v-slot:item="row">
-        <tr>
-          <td>{{ row.item.EDINumber }}</td>
-          <td>{{ row.item.foodBank }}</td>
-          <td>{{ row.item.time }}</td>
-          <td>{{ row.item.orderStatus }}</td>
-          <td>
-            <v-btn icon id="response" @click="changeStatus(row.index)">
-              <v-icon>mdi-check</v-icon>
-            </v-btn>
-            <v-btn
-              icon
-              id="response"
-              :disabled="shouldDisable(row.index)"
-              @click="removeOrder(row.index)"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </td>
-        </tr>
-      </template>
-    </v-data-table>
-  </div>
-</template>
-<script>
-export default {
-  data() {
-    return {
-      activeOrders: [
-        {
-          EDINumber: 3232,
-          foodBank: "Eden",
-          time: Date(),
-          orderStatus: "Searching for Driver",
-          items: [
-            { name: "apple", qty: 45 },
-            { name: "orange", qty: 100 },
-            { name: "banana", qty: 60 }
-          ]
-        },
-        {
-          EDINumber: 4671,
-          foodBank: "Seva",
-          time: Date(),
-          orderStatus: "Picked up",
-          items: [
-            { name: "guava", qty: 22 },
-            { name: "pineapple", qty: 500 },
-            { name: "acorn", qty: 40 }
-          ]
-        }
-      ],
-      fields: [
-        { text: "EDI Number", value: "EDINumber" },
-        { text: "Food Bank", value: "foodBank" },
-        { text: "Time", value: "time" },
-        { text: "Order Status", value: "orderStatus" },
-        { text: "", value: "response" }
-      ]
-    };
-  },
-  methods: {
-    changeStatus(index) {
-      this.activeOrders[index].orderStatus = "Picked up";
-    },
-    removeOrder(index) {
-      this.activeOrders.splice(index, 1);
-    },
-    shouldDisable(index) {
-      if (this.activeOrders[index].orderStatus == "Picked up") {
-        return true;
-      }
-      return false;
-    },
-    getInventory() {
-      
-    },
-    sendStatus() {
-
-    }
-
   }
 };
 </script>
